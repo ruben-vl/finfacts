53091b79f83e095b0068da71ca747ed2a44eaabd
ruben-vl <rubenvanlaer@mail.com>
2024-11-11 15:28:36 +0100

    save

--- passive-backtesting/history.py
+++ passive-backtesting/history.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 import yfinance as yf
 
 from requests import Session
@@ -5,13 +8,21 @@ from requests_cache import CacheMixin, SQLiteCache
 from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
 from pyrate_limiter import Duration, RequestRate, Limiter
 
-import json
 
 def save_monthly_candles(isin: str):
-    ticker = yf.Ticker(isin, session=session)
-    monthly_history = ticker.history(period="max", interval="1mo")
     data_cache_path = "/home/ruben/Projects/finfacts/cache/"
-    monthly_history.to_csv(data_cache_path + f"{isin}-p_max-int_1mo.csv", sep=',')
+    write_path = data_cache_path + f"{isin}-p_max-int_1mo.csv"
+    print(f"Saving monthly stock data for {isin}")
+    if not os.path.exists(write_path):
+        try:
+            ticker = yf.Ticker(isin, session=session)
+            monthly_history = ticker.history(period="max", interval="1mo")
+            monthly_history.to_csv(write_path, sep=',')
+            print(f"Stock data saved for {isin}")
+        except Exception as e:
+            print(f"Exception occurred for {isin}: {e}")
+    else:
+        print(f"Data for {isin} already exists")
 
 class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
     pass
@@ -22,7 +33,7 @@ session = CachedLimiterSession(
     backend=SQLiteCache("yfinance.cache")
 )
 
-with open("/home/ruben/Projects/finfacts/beursrally/assets_filtered.json", "r") as file:
+with open("/passive-backtesting/beursrally/assets_filtered.json", "r") as file:
     data = json.load(file)
     
 stock_isins = data["Stock"].keys()
